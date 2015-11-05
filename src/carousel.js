d0b2cbec4a24fbb76d82ad2f4e95f901ea91b1a8
jaketrent <trent.jake@gmail.com>
2015-11-05 07:07:35 -0700

    carousel styles

--- src/carousel.js
+++ src/carousel.js
@@ -1,4 +1,7 @@
+import Radium from 'radium'
 import React from 'react'
+
+import styles from './carousel-styles'
 
 const { node, number } = React.PropTypes
 
@@ -16,7 +19,7 @@ function renderSlides(props) {
 
 function Carousel(props) {
   return (
-    <div>
+    <div style={styles.root}>
       {renderSlides(props)}
       {props.nav}
     </div>
@@ -29,4 +32,4 @@ Carousel.propTypes = {
   width: number
 }
 
-export default Carousel
+export default Radium(Carousel)
