e8c1fd1557139c2e85c1290f166d592d6e9bf598
jaketrent <trent.jake@gmail.com>
2015-11-05 07:06:29 -0700

    frame styles

--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,12 @@
+import Radium from 'radium'
 import React from 'react'
 
-export default function Frame({ children }) {
-  return <div>{children}</div>
+import styles from './frame-styles'
+
+console.log("styles", styles)
+
+function Frame({ children }) {
+  return <div style={styles.root}>{children}</div>
 }
+
+export default Radium(Frame)
