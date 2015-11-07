6fb8cab758d0215540219a72ab03fd25d2a80163
jaketrent <trent.jake@gmail.com>
2015-11-07 15:43:54 -0700

    nav styles

--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,25 @@
+import classnames from 'classnames'
 import React from 'react'
 
 const { func, bool } = React.PropTypes
 
+function getPrevClassName(props) {
+  return classnames('dft__nav__btn', 'dft__nav__btn--prev', {
+    'dft__nav__btn--hidden': !props.hasPrevious
+  })
+}
+
+function getNextClassName(props) {
+  return classnames('dft__nav__btn', 'dft__nav__btn--next', {
+    'dft__nav__btn--hidden': !props.hasNext
+  })
+}
+
 function Nav(props) {
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div className="dft__nav">
+      <button className={getPrevClassName(props)} onClick={props.onPrevious}>&#10094;</button>
+      <button className={getNextClassName(props)} onClick={props.onNext}>&#10095;</button>
     </div>
   )
 }
