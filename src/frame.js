ca58cb6fb36bfa583152fbeab46d066f8f105d97
jaketrent <trent.jake@gmail.com>
2015-11-07 06:34:24 -0700

    frame styles

--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import styles from './frame-styles'
+
 export default function Frame({ children }) {
-  return <div>{children}</div>
+  return <div style={styles.root}>{children}</div>
 }
