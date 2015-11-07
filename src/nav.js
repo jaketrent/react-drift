5f6882e1f9e77eaccacbaae80e3c7656b6cca294
jaketrent <trent.jake@gmail.com>
2015-11-07 07:37:33 -0700

    nav styles

--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
+import styles from './nav-styles'
+
 const { func, bool } = React.PropTypes
+
+function getPrevStyles(props) {
+  return props.hasPrevious ? styles.prev : styles.prevHidden
+}
+
+function getNextStyles(props) {
+  return props.hasNext ? styles.next : styles.nextHidden
+}
 
 function Nav(props) {
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div style={styles.root}>
+      <button style={getPrevStyles(props)} onClick={props.onPrevious}>&#10094;</button>
+      <button style={getNextStyles(props)} onClick={props.onNext}>&#10095;</button>
     </div>
   )
 }
