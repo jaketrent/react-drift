acfda94c26e48fcf404a2397213e63b4ea1ed901
jaketrent <trent.jake@gmail.com>
2019-01-09 08:10:39 -0700

    upgrade m3 Before

--- zips/3-demos/Before/src/index.js
+++ zips/3-demos/Before/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import DriftApp from './app'
+import DriftApp from './app.js'
 
 ReactDOM.render(<DriftApp />, document.getElementById('app'))
